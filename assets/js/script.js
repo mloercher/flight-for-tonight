29aea9b2de471e613659c50908420fa407011cef
Rooster <gallolopez1@hotmail.com>
2021-11-15 21:34:24 -0500

    Fixed calendar in index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -109,6 +109,22 @@ function tempObj() {
     console.log(response());
 }
 
+// clear past dates in calendar
+$(function() {
+    var dtToday = new Date();
+
+    var month = dtToday.getMonth() + 1;
+    var day = dtToday.getDate();
+    var year = dtToday.getFullYear();
+    if (month < 10)
+        month = '0' + month.toString();
+    if (day < 10)
+        day = '0' + day.toString();
+
+    var maxDate = year + '-' + month + '-' + day;
+    $('#search-date').attr('min', maxDate);
+});
+
 // modal initialization
 $(document).ready(function() {
     $("#test").hide();
